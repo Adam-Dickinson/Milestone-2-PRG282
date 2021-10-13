47f5c3cdcf15b38accc7e75276d60155690624a9
Asp3ctGaming <64314687+Asp3ctGaming@users.noreply.github.com>
2021-10-13 14:39:06 +0200

    Changed Code and fixed some naming.
    I copied the code into SQL and made some changes and have pasted all the new code. All files will be uploaded as well.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,76 +1,162 @@
-use [MileStone database];
-Go
-Create procedure [AddStudent]
-@StudentNum integer,
-@Name varchar(50),
-@Surname varchar(50),
-@DOB date,
-@Gender varchar(2),
-@Phone varchar(50),
-@StreetAddress varchar(50),
-@ModuleCode varchar(6)
-as
-Insert INTO StudentInfo (StudentNum, Name, Surname, DOB, Gender, Phone, StreetAddress, ModuleCode) Values
-(@StudentNum, @Name, @Surname, @DOB, @Gender, @Phone, @StreetAddress, @ModuleCode);
-Select * From StudentInfo;
-Go
+CREATE DATABASE Milestone_2_PRG_282
 
+Create Table StudentInfo(
+StudentNum INT,
+FirstName VARCHAR(50),
+Surname VARCHAR(50),
+DOB DATE,
+Gender VARCHAR(2),
+Phone VARCHAR(10),
+StreetAddress VARCHAR(50),
+ModuleCode VARCHAR(6)
+)
 
-Go
-Create procedure [Display]
-as
+Create Table ModuleInfo(
+ModuleCode VARCHAR(6),
+ModuleName VARCHAR(50),
+ModuleDescription VARCHAR(255),
+LinkToOnlineResource VARCHAR(255)
+)
 
+// All Student Procedures
 
-Select * From StudentInfo;
+CREATE PROCEDURE spAddStudent
+(
+	@StudId INT,
+	@Name VARCHAR(50),
+	@Surname VARCHAR(50),
+	@DOB DATE,
+	@Gender VARCHAR(2),
+	@Phone VARCHAR(50),
+	@StreetAddress VARCHAR(50),
+	@ModuleCode VARCHAR(6)
+)
 
+AS
 
-Go
+BEGIN
 
-Go
-Create procedure [DeleteStudent]
-@StudentNum integer,
+INSERT INTO StudentInfo
+VALUES(@StudId, @Name, @Surname, @DOB, @Gender, @Phone, @StreetAddress, @ModuleCode)
 
-as
+END
 
-Delete (StudentNum, Name, Surname, StudentImage, DOB, Gender, Phone, StreetAddress, ModuleCode) From [StudentInfo] Where StudentNum = @StudentNum;
-Select * From StudentInfo;
+CREATE PROCEDURE spDeleteStudent
+(
+	@StudId INT
+)
 
-Go
+AS
 
-Go
-Create Procedure [UpdateStudent]
-@StudentNum integer,
-@Name varchar(50),
-@Surname varchar(50),
-@image image,
-@DOB date,
-@Gender varchar(2),
-@Phone varchar(50),
-@StreetAddress varchar(50),
-@ModuleCode varchar(6)
-as
-Update [StudentInfo] set StudentNum = @StudentNum, Name = @Name, Surname = @Surname, StudentImage = @image, DOB = @DOB, Gender = @Gender, Phone = @Phone, StreetAddress = @StreetAddress, ModuleCode = @ModuleCode
-Where StudentNum = @StudentNum;
-Select * From [StudentInfo];
+BEGIN
+	DELETE FROM StudentInfo
+	WHERE StudentNumber = @StudId
+END
 
-Go
+CREATE PROCEDURE spGetStudents
 
-Go
-Create procedure [SearchStudent]
-@StudentNum integer
-as
-Select * From StudentInfo Where StudentNum = @StudentNum;
-Go
+AS
+BEGIN
+	SELECT * FROM StudentInfo
+END
 
-Go
-Create procedure [FilterStudents]
-@ModuleCode varchar(6)
-as
-Select * from StudentInfo Where ModuleCode = @ModuleCode Order By DESC;
-Go
+CREATE PROCEDURE spSearchStudent
+(
+	@StudId INT
+)
 
-Go
-Create procedure [GroupStudents]
-as
-Select Count(StudentNum), ModuleCode From StudentInfo Group By [ModuleCode];
-Go
+AS
+BEGIN
+	SELECT * FROM StudentInfo
+	WHERE StudentNumber = @StudId
+END
+
+CREATE PROCEDURE spUpdateStudent
+(
+	@StudId INT,
+	@Name VARCHAR(50),
+	@Surname VARCHAR(50),
+	@DOB DATE,
+	@Gender VARCHAR(2),
+	@Phone VARCHAR(50),
+	@StreetAddress VARCHAR(50),
+	@ModuleCode VARCHAR(6)
+)
+
+AS
+
+BEGIN
+	UPDATE StudentInfo
+	SET StudentNumber = @StudId, FirstName = @Name, Surname = @Surname, DOB = @DOB, Gender = @Gender, PhoneNumber = @Phone, StreetAddress = @StreetAddress, ModuleCode = @ModuleCode
+	WHERE StudentNumber = @StudId
+END
+
+//All Module Procedures
+
+CREATE PROCEDURE spAddModule
+(
+	@ModuleCode VARCHAR(6),
+	@ModuleName VARCHAR(50),
+	@ModuleDescription VARCHAR(255),
+	@Link VARCHAR (255)
+)
+
+AS
+
+BEGIN
+INSERT INTO ModuleInfo
+VALUES (@ModuleCode, @ModuleName, @ModuleDescription, @Link)
+END
+
+CREATE PROCEDURE spDeleteModule
+(
+	@ModuleId VARCHAR (6)
+)
+
+AS
+BEGIN
+
+DELETE FROM ModuleInfo
+WHERE ModuleCode = @ModuleId
+
+END
+
+CREATE PROCEDURE spGetModule
+(
+	@ModuleCode VARCHAR (6)
+)
+
+AS
+BEGIN
+	SELECT * FROM ModuleInfo
+END
+
+CREATE PROCEDURE spSerachModule
+(
+	@ModuleCode VARCHAR (6)
+)
+
+AS
+
+BEGIN
+	SELECT * FROM ModuleInfo
+	WHERE ModuleCode = @ModuleCode
+END
+
+CREATE PROCEDURE spUpdateModule
+(
+	@ModuleCode VARCHAR(6),
+	@ModuleName VARCHAR(50),
+	@ModuleDescription VARCHAR(255),
+	@Link VARCHAR (255)
+)
+
+AS
+
+BEGIN
+
+	UPDATE ModuleInfo
+	SET ModuleCode = @ModuleCode, ModuleName = @ModuleName, ModuleDescription = @ModuleDescription, LinkToOnlineResource = @Link
+	WHERE ModuleCode = @ModuleCode
+
+END
